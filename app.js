07b7581c2cdd895418dc71c8eb6a1f02c21c9436
michael <michael.chen@agile-iot.net>
2020-08-01 09:48:05 +0800

    feat: modify app.js require mongoose & modify index.hbs

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const Restaurant = require('./models/restaurant')
 
 const app = express()
+
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -13,8 +16,15 @@ app.use(methodOverride('_method'))
 app.use(express.static('public'))
 
 // routes setting
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
 app.get('/', (req, res) => {
-  res.render('index')
+  Restaurant.find()
+    .lean() // 把資料轉換成單純的 JS 物件, Promise(ES6)方法
+    .sort({ _id: 'asc' }) // desc
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch((error) => console.log(error))
 })
 
 app.listen(3000, () => {

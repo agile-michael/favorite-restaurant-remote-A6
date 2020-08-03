cbae3859fb1b1f7fb6c9afe0f172b9c9393a9c9d
michael <michael.chen@agile-iot.net>
2020-08-03 09:51:28 +0800

    feat: modify edit function

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.post('/restaurants', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// 瀏覽 (Read)
+// 瀏覽 (Read) (Detail)
 app.get('/restaurants/:_id', (req, res) => {
   // console.log(req.params._id)
   const id = req.params._id
@@ -50,6 +50,37 @@ app.get('/restaurants/:_id', (req, res) => {
     .then(restaurant => res.render('show', { restaurant }))
     .catch(error => console.log(error))
 })
+
+// edit
+app.get('/restaurants/:_id/edit', (req, res) => {
+  console.log(req.params)
+  const id = req.params._id
+  return Restaurant.findById(id)
+     .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+     .catch((error) => console.log(error))
+})
+
+app.put('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  const { name, category, image, location, phone, google_map, rating, description } = req.body
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = name
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
+value = "{{restaurant.name}}"
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
